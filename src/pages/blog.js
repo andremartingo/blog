501a8e4db7836aa5d95261ff0bb0265c77ffe5b8
Andre Martingo <andre.martingo@gmail.com>
2020-07-07 19:08:49 +0200

    Remove background

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,10 +6,18 @@ import styled from "@emotion/styled"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Content = styled.div`
+const Content = styled.div`\
+  display: flex;
+  flex-wrap: wrap;
   margin: 0 auto;
   max-width: 860px;
   padding: 1.45rem 1.0875rem;
+`
+
+const Child = styled.div`
+    flex: 1 0 21%;
+    margin: 5px;
+    height: 100px;
 `
 
 const ArticleDate = styled.h5`
@@ -20,12 +28,6 @@ const ArticleDate = styled.h5`
 const MarkerHeader = styled.h3`
   display: inline;
   border-radius: 1em 0 1em 0;
-  background-image: linear-gradient(
-    -100deg,
-    rgba(255, 250, 150, 0.15),
-    rgba(255, 250, 150, 0.8) 100%,
-    rgba(255, 250, 150, 0.25)
-  );
 `
 
 const ReadingTime = styled.h5`
@@ -38,7 +40,6 @@ const IndexPage = ({ data }) => {
     <Layout>
       <SEO title="Blog" />
       <Content>
-        <h1>Blog</h1>
         {data.allMarkdownRemark.edges
           .filter(({ node }) => {
             const rawDate = node.frontmatter.rawDate
@@ -46,6 +47,7 @@ const IndexPage = ({ data }) => {
             return date < new Date()
           })
           .map(({ node }) => (
+            <Child>
             <div key={node.id}>
               <Link
                 to={node.frontmatter.path}
@@ -54,14 +56,17 @@ const IndexPage = ({ data }) => {
                   color: inherit;
                 `}
               >
+                <div>
+                  image
+                </div>
                 <MarkerHeader>{node.frontmatter.title} </MarkerHeader>
                 <div>
                   <ArticleDate>{node.frontmatter.date}</ArticleDate>
                   <ReadingTime> - {node.fields.readingTime.text}</ReadingTime>
                 </div>
-                <p>{node.excerpt}</p>
               </Link>
             </div>
+            </Child>
           ))}
       </Content>
     </Layout>
